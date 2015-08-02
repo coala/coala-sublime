fe34040833d0e5da02a9a41e90b4ea1b6b1067eb
AbdealiJK <abdealikothari@gmail.com>
2015-08-02 08:13:10 +0530

    Utils: Add retrieve_stdout and replace_stdout
    These functions are already present in
    https://gitter.im/coala-analyzer/coala. But as sublime plugins cannot
    import any pip module by default, it's been duplicated here.

--- Utils.py
+++ Utils.py
@@ -3,9 +3,36 @@ Holds various common functions and variables which will be useful in general
 by the other classes.
 """
 
+import sys
+from io import StringIO
+from contextlib import contextmanager, closing
+
 
 COALA_KEY = "coala-sublime"
 
 
 def log(*args, **kwargs):
     print(" COALA -", *args, **kwargs)
+
+
+@contextmanager
+def replace_stdout(replacement):
+    """
+    Replaces stdout with the replacement, yields back to the caller
+    and then reverts everything back.
+    """
+    _stdout = sys.stdout
+    sys.stdout = replacement
+    try:
+        yield
+    finally:
+        sys.stdout = _stdout
+
+
+@contextmanager
+def retrieve_stdout():
+    """
+    Yields a StringIO object that stdout has been redirected to.
+    """
+    with closing(StringIO()) as sio, replace_stdout(sio):
+        yield sio
