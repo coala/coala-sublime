6d16405260ee44bebd554be2710809a26537f3bc
AbdealiJK <abdealikothari@gmail.com>
2015-08-02 08:13:39 +0530

    UtilsTest: Add tests for Utils

---
+++ tests/UtilsTest.py
@@ -0,0 +1,19 @@
+import unittest
+
+from Utils import log, retrieve_stdout
+
+
+class UtilsTest(unittest.TestCase):
+    def test_retrieve_stdout(self):
+        with retrieve_stdout() as sio:
+            print("test")
+            self.assertEqual(sio.getvalue(), "test\n")
+
+    def test_log(self):
+        with retrieve_stdout() as sio:
+            log("test")
+            self.assertEqual(sio.getvalue(), " COALA - test\n")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
